35579ae7a01e1bcb73885cfbe51fcfe975369289
mert arın <mertkarin2@gmail.com>
2024-01-15 12:21:37 -0600

    onbesocak

---
+++ abc/helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main (){
+    std::cout << "Hello world!";
+    return 0;
+}
+// Interesting fact 1) I am an insane fan of Turkish football team called Fenerbahçe.
+// Interesting fact 2) My favourite meal is french fries with egg on top.
+// Interesting fact 3) I love small cities like Jacksonville AL more than big cities like Atlanta.
+// Interesting fact 4) When I was a little kid, my biggest fear was falling into spaces of subway stations.
+// Interesting fact 5) In my hometown, I walked 10 subway stations long way to explore city more. I love walking through cities.
